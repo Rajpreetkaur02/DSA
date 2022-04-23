e95a2a03184ab41c64a48c26acfda2b3d84d561d
Rajpreetkaur02 <78468285+Rajpreetkaur02@users.noreply.github.com>
2022-04-23 23:17:04 +0530

    Update rotate_number.cpp

--- basics_of_java/getting_started_questions/rotate_number.cpp
+++ basics_of_java/getting_started_questions/rotate_number.cpp
@@ -1,1 +1,30 @@
+public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        int k=scn.nextInt();
+        int temp=n;
+        int count=0;
+        while(temp!=0){
+            temp=temp/10;
+            count++;
+        }
+        k=k%count;
+        if(k<0){
+            k=k+count;
+        }
+        int div=1;
+        int mul=1;
+        for(int i=1;i<=count;i++){
+            if(i<=k){
+                div=div*10;
+            }
+            else{
+                mul=mul*10;
+            }    
+        }
+        int q=n/div;
+        int r=n%div;
+        int rot=r*mul+q;
+        System.out.println(rot);
+    }
 
