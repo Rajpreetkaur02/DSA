38c412678b8996a8e0e9bdf3be47c6df89c1d0ee
Rajpreetkaur02 <78468285+Rajpreetkaur02@users.noreply.github.com>
2022-04-23 23:20:03 +0530

    Update IsPrime.cpp

--- basics_of_java/getting_started_questions/IsPrime.cpp
+++ basics_of_java/getting_started_questions/IsPrime.cpp
@@ -1,1 +1,22 @@
-
+public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+       int t=scn.nextInt();
+       
+       for(int i=0;i<t;i++){
+         boolean flag=true;
+         int n=scn.nextInt();
+         for(int div=2;(div*div)<=n;div++){
+           if(n%div==0){
+             flag=false;
+             break;
+           }
+         }
+       if(flag){
+         System.out.println("prime");
+       }
+       else{
+         System.out.println("not prime");
+       }
+       }
+       
+   }
