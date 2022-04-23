2bee7ea7021525521fce1e526fdb6fe6c09a661e
Rajpreetkaur02 <78468285+Rajpreetkaur02@users.noreply.github.com>
2022-04-23 23:22:05 +0530

    Update Fibonacci_till_N.cpp

--- basics_of_java/getting_started_questions/Fibonacci_till_N.cpp
+++ basics_of_java/getting_started_questions/Fibonacci_till_N.cpp
@@ -1,1 +1,15 @@
-
+//0 1 1 2 3 5 8 13....
+public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int n=scn.nextInt();
+      int a=0;
+      int b=1;
+      System.out.println(a);
+      System.out.println(b);
+      for(int i=0;i<n-2;i++){
+        int next_term=a+b;
+        System.out.println(next_term);
+        a=b;
+        b=next_term;
+      }
+   }
